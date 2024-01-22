0b0e66ae1efbff7bbf1dd5e30277f92e321818e6
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-01-22 15:47:36 +0200

    add public varialbes and igone underline char in copy field names

--- Editor/ReflectionExtensions.cs
+++ Editor/ReflectionExtensions.cs
@@ -17,12 +17,19 @@ namespace ManagedReference
             var toAllFields = to.GetType().GetAllSerializedFields();
             foreach (var toField in toAllFields)
             {
-                var referenceField = fromAllFields.Find(x => x.Name == toField.Name);
+                var referenceField = fromAllFields.Find(x => IsSameFieldName(x.Name,toField.Name));
                 if (referenceField != null)
                 {
                     CopyValue(referenceField, from, toField, to);
                 }
             }
+        }
+
+        private static bool IsSameFieldName(string name1, string name2)
+        {
+            name1 = name1.Replace("_", string.Empty);
+            name2 = name2.Replace("_", string.Empty);
+            return name1 == name2;
         }
 
         private static void CopyValue(FieldInfo fromField, object from, FieldInfo toField, object to)
@@ -53,7 +60,7 @@ namespace ManagedReference
             BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
             => type
                 .GetFields(flags)
-                .Where(x => x.GetCustomAttributes().OfType<SerializeField>().Any())
+                .Where(x => x.GetCustomAttributes().OfType<SerializeField>().Any() || x.IsPublic)
                 .ToList();
     }
 }
